e7e5464722b35972dae64742f495fa3bb18952a6
Vamshi Gopari <47558150+GopariVAmshi@users.noreply.github.com>
2020-02-05 21:00:35 +0530

    Java-annotations-beansScope-&-Lifecycle
    Java-annotations-beansScope-&-Lifecycle

---
+++ 06-Java-annotations-beanScope-&-Lifecycle/src/com/vamshi/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.vamshi.springdemo;
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+	public String getDailyFortune();
+}
---
+++ 06-Java-annotations-beanScope-&-Lifecycle/src/com/vamshi/springdemo/FileFortuneService.java
@@ -0,0 +1,57 @@
+package com.vamshi.springdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.io.*;
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileFortuneService implements FortuneService {
+	
+	private String fileName="fortune-data.txt";
+	private List<String>theFortunes;
+	
+	private Random myRandom = new Random();
+	
+	public FileFortuneService() {
+		System.out.println(">>FileFortuneService: inside default constructor\n");
+	}
+	
+	@PostConstruct
+	private void loadTheFortuneFile() {
+		
+		System.out.println(">>FileFortuneService: inside method loadTheFortuneFile\n");
+		
+		File theFile=new File(fileName);
+		
+		System.out.println("Reading fortune from file:"+theFile+"\n");
+		System.out.println("File exists:"+theFile.exists()+"\n");
+		
+		theFortunes = new ArrayList<String>();
+		
+		try(BufferedReader br=new BufferedReader(new FileReader(theFile))){
+			
+			String tempLine;
+			
+			while((tempLine = br.readLine()) != null) {
+				theFortunes.add(tempLine);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public String getFortune() {
+		
+		int index = myRandom.nextInt(theFortunes.size());
+		
+		String tempFortune = theFortunes.get(index);
+		
+		return tempFortune+"\n";
+	}
+
+}
